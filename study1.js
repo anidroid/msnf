eead64ae0c5021d85249a297b3d8f2212780c51d
AL <alevordashka@gmail.com>
2017-06-20 11:19:07 +0100

    hide prog bar

--- study1.js
+++ study1.js
@@ -220,7 +220,7 @@ $(document).ready(function() {
         });
 
         routie('end', function() {
-
+            $('.progress-bar').hide();
             $('.undercover').hide();
             $('#debrief').show();
             $('#dtext1').empty().append($.parseHTML(design.instr.debriefing.text1));
